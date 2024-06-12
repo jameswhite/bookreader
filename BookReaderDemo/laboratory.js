3084f86c5d7132a0676b764ed4afc2e20bbecc94
James S. White <james@jameswhite.org>
2024-06-12 11:46:57 -0500

    make it as simple as possible first

--- BookReaderDemo/laboratory.js
+++ BookReaderDemo/laboratory.js
@@ -1,7 +1,3 @@
-// Import the node-fetch module
-const fetch = require('node-fetch');
-
-// Define the URL of the remote JSON file
 const url = 'https://127.0.0.1:8000/BookReaderDemo/laboratory.json';
 
 // Create the BookReader object
@@ -9,16 +5,10 @@ function instantiateBookReader(selector, extraOptions) {
   selector = selector || '#BookReader';
   extraOptions = extraOptions || {};
 
-// Fetch the JSON file and parse it into a JavaScript object
-fetch(url)
-  .then(response => response.json())
-  .then(options => {
-    // The 'data' variable contains the parsed JSON as a JavaScript object
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching the JSON file:', error);
-  });
+  // Fetch the JSON file and parse it into a JavaScript object
+  const response = await fetch(url);
+  const options = await response.json();
+
   $.extend(options, extraOptions);
   var br = new BookReader(options);
   br.init();
