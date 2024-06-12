da8c3f2613bd54c85e18cbb50ff2233fc2a93a55
James S. White <james@jameswhite.org>
2024-06-12 11:58:36 -0500

    pad the scope

--- BookReaderDemo/laboratory.js
+++ BookReaderDemo/laboratory.js
@@ -1,10 +1,10 @@
 const url = 'http://127.0.0.1:8000/BookReaderDemo/laboratory.json';
+options = {}
 
 // Create the BookReader object
 function instantiateBookReader(selector, extraOptions) {
   selector = selector || '#BookReader';
   extraOptions = extraOptions || {};
-  options = options || {};
 
   // Fetch the JSON file and parse it into a JavaScript object
   fetch(url, { method: 'GET' }).then(function(response) { return response.json(); }).then(function(json) { options = json; });
