55eb092d75205fe303fb966d6831f7e0ab5004ac
James S. White <james@jameswhite.org>
2024-06-12 13:49:26 -0500

    clean up the logging

--- BookReaderDemo/laboratory.js
+++ BookReaderDemo/laboratory.js
@@ -10,11 +10,9 @@ function instantiateBookReader(selector, extraOptions) {
   fetch(url, { method: 'GET' })
     .then(function(response) { return response.json(); })
     .then( function(json) {
-      console.log(json); options = json; console.log(options); 
-  console.log(options);
-  $.extend(options, extraOptions);
-  var br = new BookReader(options);
-  br.init();
-  });
-
+       options = json;
+       console.log(options);
+       $.extend(options, extraOptions);
+       var br = new BookReader(options);
+       br.init(); });
 }
